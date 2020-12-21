0b7139baeee85c727bf00343455d2f4658197f8e
LilMaksek <maksim_gavrilov_2014@bk.ru>
2020-12-21 17:54:13 +0400

    Update stupid mistakes

--- VK_Maksim_func/VK_API/Friend.cs
+++ VK_Maksim_func/VK_API/Friend.cs
@@ -18,20 +18,7 @@ namespace VK_API
 
         public static List<JSON_FriendID_online> GetIDFriendsOnline()
             
-        {   //"https://api.vk.com/method/friends.getOnline?v=5.52&access_token="+
-
-            //WebRequest request = WebRequest.Create("https://api.vk.com/method/friends.get?user_id=201272223&fields=first_name&access_token=f624c346e70d72701dea39f39e08d7b315939f17feab81f4c545fc4c24e2a4d351f26c47054d81b091134&v=5.126");
-            // If required by the server, set the credentials.
-            //request.Credentials = CredentialCache.DefaultCredentials;
-            // Get the response.
-            //HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            // Read response
-            //StreamReader sr = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
-            //Parse response
-
-            //var parsed = JsonConvert.DeserializeObject<List<JSON_FriendID_online>>(str);
-
-            //Console.WriteLine(parsed[0].first_name);
+        {   
             var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://api.vk.com/method/friends.get?user_id=201272223&fields=first_name&access_token=f624c346e70d72701dea39f39e08d7b315939f17feab81f4c545fc4c24e2a4d351f26c47054d81b091134&v=5.126");
 
             
@@ -43,12 +30,11 @@ namespace VK_API
             string result = streamReader.ReadToEnd();
 
             
-            result.Remove(0, 33);
-            result.Remove(result.Length - 2,2);
-            string rez = result.Remove(0, 33);
-            string rez2 = rez.Remove(rez.Length - 2, 2);
+            result = result.Remove(0, 33);
+            result = result.Remove(result.Length - 2, 2);
+            
             //Console.WriteLine(rez2);
-            var rootObject = JsonConvert.DeserializeObject<List<JSON_FriendID_online>>(rez2);
+            var rootObject = JsonConvert.DeserializeObject<List<JSON_FriendID_online>>(result);
 
             Console.WriteLine(rootObject[0].first_name);
 
@@ -60,10 +46,7 @@ namespace VK_API
             
         }
 
-        public void appendFriends (Form2 form)
-        {
-            
-        }
+        
 
         static void main(String [] args)
         {
--- VK_Maksim_func/VK_API/Gift.cs
+++ VK_Maksim_func/VK_API/Gift.cs
@@ -11,7 +11,7 @@ namespace VK_API
 {
     class Gift
     {
-        //List<JSON_Gift>
+        
         public static List<JSON_Gift>  getGifts ()
         {
             var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://api.vk.com/method/gifts.get?&access_token=f624c346e70d72701dea39f39e08d7b315939f17feab81f4c545fc4c24e2a4d351f26c47054d81b091134&v=5.126");
